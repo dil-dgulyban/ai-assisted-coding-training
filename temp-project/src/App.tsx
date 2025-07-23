bf9462908d6f2c557454c69e049c0fccb2dd64cf
ddaradics <ddaradics@diligent.com>
2025-07-23 18:31:36 +0200

    Task 1: Set up React project with required dependencies

---
+++ temp-project/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Todo App</h1>
+      {/* App content will go here in future tasks */}
+    </div>
+  )
+}
+
+export default App
