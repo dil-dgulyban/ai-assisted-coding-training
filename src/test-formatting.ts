a624d803766502c48ad9361082acbc34294e734e
ddaradics <ddaradics@diligent.com>
2025-07-24 12:03:10 +0200

    test: Add test file for Prettier

---
+++ src/test-formatting.ts
@@ -0,0 +1,9 @@
+// This is a test file to check the formatting and linting setup
+
+function testFunction(param1: string, param2: any) {
+  if (param1 === 'test') {
+    return true;
+  } else {
+    return false;
+  }
+}
